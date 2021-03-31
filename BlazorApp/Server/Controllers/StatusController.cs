e68893671c74e83fb6650e0ec6ebf8e3bcdc07f0
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-31 17:03:21 -0400

    done

---
+++ BlazorApp/Server/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Server.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+
+        [HttpGet("/status")]
+        public ActionResult GetTheServerStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                StatusMessage = "Looks Good Here!",
+                LastChecked = DateTime.Now,
+                CheckedBy = "Fred"
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ BlazorApp/Shared/GetStatusResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class GetStatusResponse
+    {
+        public string StatusMessage { get; set; }
+        public DateTime LastChecked { get; set; }
+        public string CheckedBy { get; set; }
+    }
+}
